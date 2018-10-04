3ec861660c6c7171a0885580f4854aff1c79103c
Christoph Wurst <christoph@winzerhof-wurst.at>
2018-10-04 15:13:18 +0200

    Add debug logging to telegram gateway
    Signed-off-by: Christoph Wurst <christoph@winzerhof-wurst.at>

--- lib/Service/Gateway/Telegram/Gateway.php
+++ lib/Service/Gateway/Telegram/Gateway.php
@@ -30,6 +30,7 @@ use OCA\TwoFactorGateway\Service\Gateway\IGatewayConfig;
 use OCP\Http\Client\IClient;
 use OCP\Http\Client\IClientService;
 use OCP\IConfig;
+use OCP\ILogger;
 use OCP\IUser;
 use Telegram\Bot\Api;
 use Telegram\Bot\Objects\Update;
@@ -45,12 +46,17 @@ class Gateway implements IGateway {
 	/** @var IConfig */
 	private $config;
 
+	/** @var ILogger */
+	private $logger;
+
 	public function __construct(IClientService $clientService,
 								GatewayConfig $gatewayConfig,
-								IConfig $config) {
+								IConfig $config,
+								ILogger $logger) {
 		$this->client = $clientService->newClient();
 		$this->gatewayConfig = $gatewayConfig;
 		$this->config = $config;
+		$this->logger = $logger;
 	}
 
 	/**
@@ -61,28 +67,36 @@ class Gateway implements IGateway {
 	 * @throws SmsTransmissionException
 	 */
 	public function send(IUser $user, string $identifier, string $message) {
+		$this->logger->debug("sending telegram message to $identifier, message: $message");
 		$botToken = $this->gatewayConfig->getBotToken();
+		$this->logger->debug("telegram bot token: $botToken");
 
 		$api = new Api($botToken);
 		$chatId = $this->getChatId($user, $api, (int)$identifier);
 
+		$this->logger->debug("sending telegram message to chat $chatId");
 		$api->sendMessage([
 			'chat_id' => $chatId,
 			'text' => $message,
 		]);
+		$this->logger->debug("telegram message to chat $chatId sent");
 	}
 
 	private function getChatId(IUser $user, Api $api, int $userId): int {
 		$chatId = $this->config->getUserValue($user->getUID(), 'twofactor_gateway', 'telegram_chat_id', null);
 
 		if (!is_null($chatId)) {
+			$this->logger->debug("using cached telegram chat id $chatId for user $userId");
 			return (int)$chatId;
 		}
 
+		$this->logger->debug("trying to get chat id from updates");
 		$updates = $api->getUpdates();
+		$this->logger->debug("got " . count($updates) . " updates");
 		/** @var Update $update */
 		$update = current(array_filter($updates, function (Update $data) use ($userId) {
 			if ($data->message->text === "/start" && $data->message->from->id === $userId) {
+				$this->logger->debug("found `/start` message for user $userId");
 				return true;
 			}
 			return false;
@@ -91,6 +105,7 @@ class Gateway implements IGateway {
 
 		$chatId = $update->message->chat->id;
 		$this->config->setUserValue($user->getUID(), 'twofactor_gateway', 'telegram_chat_id', $chatId);
+		$this->logger->debug("found chat id $chatId for user $userId");
 
 		return (int)$chatId;
 	}
