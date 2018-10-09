8bab6ebbd1ed402ca7927bf1776b479c0e4ae59a
Manuel <mmartinortiz@gmail.com>
2018-10-09 20:51:37 +0200

    More detailed steps for configuring telegram

--- js/components/TelegramInstructions.vue
+++ js/components/TelegramInstructions.vue
@@ -1,13 +1,19 @@
 <template>
 	<div>
 		<p>
-			In order to receive authentication codes via Telegram, you first
-			have to start a new chat with the bot set up by your admin.<br>
-			Secondly, you have to obtain your Telegram ID via the <a
-				href="https://telegram.me/get_id_bot"
-				target="_blank"
-				rel="noreferrer noopener">ID Bot</a>.
-			Enter this ID to receive your verification code below.
+			In order to receive authentication codes via Telegram, your admin needs
+			to setup a Telegram Bot that will send the authentication codes to you.
+			Once this has been done:<br>
+			<ol>
+			  <li>Start a conversation with the Bot. You can ask your admin for
+				the bot name.</li>
+				<li>You need your Chat Id. You can get it sending a message to the <a
+					href="https://telegram.me/get_id_bot"
+					target="_blank"
+					rel="noreferrer noopener">ID Bot</a>.</li>
+				<li>Introduce your Chat Id in the following box. You will receive a
+				confirmation message from the bot.</li>
+			</ol>
 		</p>
 	</div>
 </template>
