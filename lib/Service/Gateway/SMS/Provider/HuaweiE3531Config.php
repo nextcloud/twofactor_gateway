066247dd0ee1bd59145c121215f0051b0bd7b0b4
Martin Keßler <martin@moegger.de>
2019-03-20 21:21:36 +0100

    add provider to send SMS via Huawei E3531 3G stick

---
+++ lib/Service/Gateway/SMS/Provider/HuaweiE3531Config.php
@@ -0,0 +1,63 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * @author Martin Keßler <martin@moegger.de>
+ *
+ * Nextcloud - Two-factor Gateway
+ *
+ * This code is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License, version 3,
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ *
+ */
+
+namespace OCA\TwoFactorGateway\Service\Gateway\SMS\Provider;
+
+use function array_intersect;
+use OCA\TwoFactorGateway\AppInfo\Application;
+use OCA\TwoFactorGateway\Exception\ConfigurationException;
+use OCP\IConfig;
+
+class HuaweiE3531Config implements IProviderConfig {
+
+	/** @var IConfig */
+	private $config;
+
+	public function __construct(IConfig $config) {
+		$this->config = $config;
+	}
+
+	private function getOrFail(string $key): string {
+		$val = $this->config->getAppValue(Application::APP_NAME, $key, null);
+		if (is_null($val)) {
+			throw new ConfigurationException();
+		}
+		return $val;
+	}
+
+	public function getUrl(): string {
+		return $this->getOrFail('huawei_e3531_api');
+	}
+
+	public function setUrl(string $url) {
+		$this->config->setAppValue(Application::APP_NAME, 'huawei_e3531_api', $url);
+	}
+
+	public function isComplete(): bool {
+		$set = $this->config->getAppKeys(Application::APP_NAME);
+		$expected = [
+			'huawei_e3531_api',
+		];
+		return count(array_intersect($set, $expected)) === count($expected);
+	}
+}
