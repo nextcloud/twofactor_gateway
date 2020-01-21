10c002a96919aba4d74eb681642c3b2e6b9fb6c7
Jordan Bieder <jordan.bieder@geduld.fr>
2020-01-21 22:50:55 +0100

    What happened to this file ?!
    Signed-off-by: Jordan Bieder <jordan.bieder@geduld.fr>

--- lib/Command/Configure.php
+++ lib/Command/Configure.php
@@ -107,7 +107,7 @@ class Configure extends Command {
 	private function configureSms(InputInterface $input, OutputInterface $output) {
 		$helper = $this->getHelper('question');
 
-		$providerQuestion = new Question('Please choose a SMS provider (websms, playsms, clockworksms, puzzelsms, ecallsms, voipms, huawei_e3531, spryng, sms77io, ovh'): ', 'websms');
+		$providerQuestion = new Question('Please choose a SMS provider (websms, playsms, clockworksms, puzzelsms, ecallsms, voipms, huawei_e3531, spryng, sms77io, ovh', 'websms');
 		$provider = $helper->ask($input, $output, $providerQuestion);
 
 		/** @var SMSConfig $config */
