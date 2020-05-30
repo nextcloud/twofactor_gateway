dab96f04407649fca90d0eee62d08f2595175e84
Chris Jenkins <chris.j@guruinventions.com>
2020-05-30 01:19:21 +0000

    1st draft impl of Plivo SMS provider

---
+++ lib/Service/Gateway/SMS/Provider/PlivoConfig.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * @author Chris Jenkins <chris.j@guruinventions.com>
+ *
+ * Plivo - Config for Two-factor Gateway for Plivo
+ *
+ * This code is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License, version 3,
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ *
+ */
+
+namespace OCA\TwoFactorGateway\Service\Gateway\SMS\Provider;
+
