c0a262f67e08d8ce207900c7ac64b20d82641b2d
Rainer Dohmen <rdohmen@pensionmoselblick.de>
2023-02-20 19:27:09 +0100

    XMPP Vue Components added
    Signed-off-by: Rainer Dohmen <rdohmen@pensionmoselblick.de>

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import Vue from "vue"
 import SignalSettings from "./views/SignalSettings.vue"
 import SMSSettings from "./views/SMSSettings.vue"
 import TelegramSettings from "./views/TelegramSettings.vue"
+import XMPPSettings from "./views/XMPPSettings.vue"
 
 Vue.config.productionTip = false
 
@@ -17,3 +18,7 @@ new Vue({
 new Vue({
     render: h => h(TelegramSettings)
 }).$mount('#twofactor-gateway-telegram')
+
+new Vue({
+    render: h => h(XMPPSettings)
+}).$mount('#twofactor-gateway-xmpp')
