43e4db8106ecb15b648e11c14edf97f831df1f4e
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2025-09-09 18:03:46 -0300

    chore: start to implement psalm
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

---
+++ tests/stubs/oc_hooks_emitter.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * SPDX-FileCopyrightText: 2025 LibreCode coop and contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+namespace OC\Hooks {
+	class Emitter {
+		public function emit(string $class, string $value, array $option):void {
+		}
+
+		public function listen(string $class, string $value, $closure):void {
+		}
+	}
+}
