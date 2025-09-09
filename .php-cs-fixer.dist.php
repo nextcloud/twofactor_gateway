dbc3b646e75a294ec6716615b82d39e3afacda9d
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2025-09-09 16:03:01 -0300

    chore: Bump PHP dependencies
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,17 +1,24 @@
 <?php
 
 declare(strict_types=1);
+/**
+ * SPDX-FileCopyrightText: 2025 LibreCode coop and contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
 
-require_once './vendor/autoload.php';
+require_once './vendor-bin/csfixer/vendor/autoload.php';
 
 use Nextcloud\CodingStandard\Config;
+use PhpCsFixer\Runner\Parallel\ParallelConfigFactory;
 
 $config = new Config();
 $config
+	->setParallelConfig(ParallelConfigFactory::detect())
 	->getFinder()
-	->notPath('build')
+	->ignoreVCSIgnored(true)
 	->notPath('l10n')
 	->notPath('src')
 	->notPath('vendor')
+	->notPath('vendor-bin')
 	->in(__DIR__);
 return $config;
