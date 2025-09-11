fd96f110d70f1a3def4e00277f0fab92c7f72335
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2025-09-11 13:10:12 -0300

    feat: start to implement OpenAPI
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

---
+++ lib/ResponseDefinitions.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * SPDX-FileCopyrightText: 2025 LibreCode coop and contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+namespace OCA\TwoFactorGateway;
+
+/**
+ * @psalm-type TwoFactorGatewayCapabilities = array{
+ *     features: list<string>,
+ *     config: array{
+ *     },
+ *     version: string,
+ * }
+ */
+final class ResponseDefinitions {
+}
