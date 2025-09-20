0614a50455845782659e8a3e30829a9716aa50aa
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2025-09-20 09:49:03 -0300

    chore: make the provider factory more generic
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

--- lib/Provider/Factory.php
+++ lib/Provider/Factory.php
@@ -10,27 +10,72 @@ declare(strict_types=1);
 namespace OCA\TwoFactorGateway\Provider;
 
 use OCA\TwoFactorGateway\Exception\InvalidProviderException;
+use OCP\Server;
 
 class Factory {
-
-	public function __construct(
-		private SignalProvider $signalProvider,
-		private SmsProvider $smsProvider,
-		private TelegramProvider $telegramProvider,
-		private XMPPProvider $xmppProvider,
-	) {
-	}
+	/** @var array<string,AProvider> */
+	private array $instances = [];
+	/** @var array<string> */
+	private array $fqcn = [];
 
 	/**
 	 * @throws InvalidProviderException
 	 */
 	public function getProvider(string $name): AProvider {
-		return match (strtolower($name)) {
-			'signal' => $this->signalProvider,
-			'sms' => $this->smsProvider,
-			'telegram' => $this->telegramProvider,
-			'xmpp' => $this->xmppProvider,
-			default => throw new InvalidProviderException(),
-		};
+		$needle = strtolower($name);
+		if (isset($this->instances[$needle])) {
+			return $this->instances[$needle];
+		}
+
+		foreach ($this->getFqcnList() as $fqcn) {
+			$type = $this->typeFrom($fqcn);
+			if ($type !== $needle) {
+				continue;
+			}
+			/** @var AProvider */
+			$instance = Server::get($fqcn);
+			$this->instances[$needle] = $instance;
+			return $instance;
+		}
+		throw new InvalidProviderException();
+	}
+
+	private function getFqcnList(): array {
+		if (!empty($this->fqcn)) {
+			return $this->fqcn;
+		}
+
+		foreach ($this->getClassMap() as $fqcn => $_) {
+			$type = $this->typeFrom($fqcn);
+			if ($type === null) {
+				continue;
+			}
+			if (!is_subclass_of($fqcn, AProvider::class, true)) {
+				continue;
+			}
+			$this->fqcn[] = $fqcn;
+		}
+
+		return $this->fqcn;
+	}
+
+	private function getClassMap(): array {
+		$loader = require __DIR__ . '/../../vendor/autoload.php';
+		return $loader->getClassMap();
+	}
+
+	private function typeFrom(string $fqcn): ?string {
+		$prefix = 'OCA\\TwoFactorGateway\\Provider\\';
+		if (strncmp($fqcn, $prefix, strlen($prefix)) !== 0) {
+			return null;
+		}
+
+		$rest = substr($fqcn, strlen($prefix));
+		if (strpos($rest, '\\') === false && !str_ends_with($rest, 'Provider')) {
+			return null;
+		}
+
+		$type = substr($rest, 0, -strlen('Provider'));
+		return $type !== '' ? strtolower($type) : null;
 	}
 }
