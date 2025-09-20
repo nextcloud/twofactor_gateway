7b1c809667057173ac6715c00ddf869c93caf569
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2025-09-20 15:04:33 -0300

    chore: make the remove command generic
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

--- lib/Command/Remove.php
+++ lib/Command/Remove.php
@@ -12,27 +12,40 @@ namespace OCA\TwoFactorGateway\Command;
 use Exception;
 use OCA\TwoFactorGateway\Provider\Gateway\Factory;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Helper\QuestionHelper;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\ChoiceQuestion;
 
 class Remove extends Command {
+	private array $ids = [];
 
 	public function __construct(
 		private Factory $gatewayFactory,
 	) {
 		parent::__construct('twofactorauth:gateway:remove');
 
+		$fqcn = $this->gatewayFactory->getFqcnList();
+		foreach ($fqcn as $fqcn) {
+			$this->ids[] = $fqcn::getProviderId();
+		}
+
 		$this->addArgument(
 			'gateway',
-			InputArgument::REQUIRED,
-			'The name of the gateway, e.g. sms, signal, telegram, xmpp, etc.'
+			InputArgument::OPTIONAL,
+			'The name of the gateway: ' . implode(', ', $this->ids)
 		);
 	}
 
 	#[\Override]
 	protected function execute(InputInterface $input, OutputInterface $output) {
 		$gatewayName = strtolower((string)$input->getArgument('gateway'));
+		if (!in_array($gatewayName, $this->ids, true)) {
+			$helper = new QuestionHelper();
+			$choiceQuestion = new ChoiceQuestion('Please choose a provider:', $this->ids);
+			$gatewayName = $helper->ask($input, $output, $choiceQuestion);
+		}
 
 		try {
 			$gateway = $this->gatewayFactory->get($gatewayName);
