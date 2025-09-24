2309f203ecfee1623e050450ede657cb996f0e1f
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2025-09-24 15:39:46 -0300

    fix: replace by match
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

--- lib/Service/SetupService.php
+++ lib/Service/SetupService.php
@@ -59,9 +59,10 @@ class SetupService {
 		$verificationNumber = $this->random->generate(6, ISecureRandom::CHAR_DIGITS);
 		$gateway = $this->gatewayFactory->get($gatewayName);
 		try {
-			$message = $gateway->getSettings()['allow_markdown'] ?? false
-				? $this->l10n->t('`%s` is your verification code.', [$verificationNumber])
-				: $this->l10n->t('%s is your verification code.', [$verificationNumber]);
+			$message = match ($gateway->getSettings()['allow_markdown'] ?? false) {
+				true => $this->l10n->t('`%s` is your verification code.', [$verificationNumber]),
+				default => $this->l10n->t('%s is your verification code.', [$verificationNumber]),
+			};
 			$gateway->send(
 				$identifier,
 				$message,
