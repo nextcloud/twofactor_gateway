b77dea8ea4680a57bdbbd7dadb76db810cd2b955
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2025-09-24 15:17:36 -0300

    chore: configure bot
    Was necessary to run as separated command before update Nextcloud to
    PHP8.2
    
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

--- lib/Provider/Channel/Telegram/Gateway.php
+++ lib/Provider/Channel/Telegram/Gateway.php
@@ -58,6 +58,16 @@ class Gateway extends AGateway {
 	}
 
 	#[\Override]
+	public function getSettings(): array {
+		try {
+			$provider = $this->getProvider();
+		} catch (ConfigurationException) {
+			return static::SCHEMA;
+		}
+		return $provider::SCHEMA;
+	}
+
+	#[\Override]
 	public function isComplete(array $schema = []): bool {
 		if (empty($schema)) {
 			try {
