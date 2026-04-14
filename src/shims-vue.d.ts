3269352ab864adf51815b199c03c8922cb50eda7
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2026-04-14 16:20:29 -0300

    feat(types): add Vue SFC shims for TypeScript
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

---
+++ src/shims-vue.d.ts
@@ -0,0 +1,11 @@
+/**
+ * SPDX-FileCopyrightText: 2026 LibreCode coop and contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+declare module '*.vue' {
+	import type { DefineComponent } from 'vue'
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/ban-types
+	const component: DefineComponent<{}, {}, any>
+	export default component
+}
