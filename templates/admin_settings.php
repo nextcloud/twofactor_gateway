cfad3b6aa8ee3bb95aca6d25eb75c6519ad28ae6
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2026-04-14 16:35:15 -0300

    fix(templates): add missing addScript call to load admin JS bundle
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

--- templates/admin_settings.php
+++ templates/admin_settings.php
@@ -3,5 +3,7 @@
  * SPDX-FileCopyrightText: 2026 LibreCode coop and contributors
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
+
+\OCP\Util::addScript('twofactor_gateway', 'twofactor_gateway-admin');
 ?>
 <div id="twofactor-gateway-admin"></div>
