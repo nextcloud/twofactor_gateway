9222f594cb748ce1860ec8d68b353f49b2bbdc8d
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2026-04-16 19:06:50 -0300

    refactor(ui): remove routing controls from provider edit modal
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

--- src/components/GatewayInstanceModal.vue
+++ src/components/GatewayInstanceModal.vue
@@ -104,39 +104,6 @@
 				</div>
 			</template>
 
-			<template v-if="instanceId && !wizardPanelActive">
-				<div class="modal-divider" />
-
-				<div class="modal-field">
-					<NcTextField
-						:model-value="String(priority)"
-						:label="t('twofactor_gateway', 'Routing priority')"
-						:placeholder="t('twofactor_gateway', '0 = default order, higher runs first')"
-						:error="!!errors.priority"
-						:helper-text="errors.priority ?? t('twofactor_gateway', 'Use this to define fallback order among matched instances.')"
-						@update:modelValue="onPriorityChange" />
-				</div>
-
-				<div v-if="groups.length > 0" class="modal-field">
-					<label for="routing-groups-select">{{ t('twofactor_gateway', 'Routing groups') }}</label>
-					<NcSelect
-						input-id="routing-groups-select"
-						v-model="selectedGroups"
-						:options="groups"
-						:placeholder="t('twofactor_gateway', 'Restrict to groups\u00A0\u2026')"
-						label="displayName"
-						track-by="id"
-						:multiple="true"
-						:keep-open="true"
-						:deselect-from-dropdown="true"
-						:close-on-select="false" />
-					<small class="modal-help-text">
-						{{ t('twofactor_gateway', 'If no group is selected, this instance participates only in default fallback ordering.') }}
-					</small>
-				</div>
-			</template>
-
-
 			<component
 				:is="gatewaySetupPanel"
 				v-if="gatewaySetupPanel"
