dc60bcfb17e689a727eb169fa1b9cce634b56d55
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2026-04-17 00:19:45 -0300

    test(status): add verbose output covers instance registry data
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

--- tests/php/Unit/Command/StatusTest.php
+++ tests/php/Unit/Command/StatusTest.php
@@ -10,6 +10,7 @@ declare(strict_types=1);
 namespace OCA\TwoFactorGateway\Tests\Unit\Command;
 
 use OC\Console\Application;
+use OCA\TwoFactorGateway\AppInfo\Application as TwoFactorGatewayApplication;
 use OCA\TwoFactorGateway\Tests\Unit\AppTestCase;
 use OCP\Server;
 use Symfony\Component\Console\Input\ArrayInput;
@@ -36,5 +37,40 @@ class StatusTest extends AppTestCase {
 				$this->assertEmpty($value);
 			}
 		}
+	}
+
+	public function testExecuteVerboseShowsInstanceRegistryData(): void {
+		$this->makeInMemoryAppConfig();
+
+		self::$store[TwoFactorGatewayApplication::APP_ID]['instances:signal'] = json_encode([
+			[
+				'id' => 'signal-instance-1',
+				'label' => 'Signal primary',
+				'default' => true,
+				'createdAt' => '2026-04-17T00:00:00+00:00',
+				'groupIds' => ['admins'],
+				'priority' => 10,
+			],
+		], JSON_THROW_ON_ERROR);
+		self::$store[TwoFactorGatewayApplication::APP_ID]['signal:signal-instance-1:url'] = 'http://localhost:5000';
+		self::$store[TwoFactorGatewayApplication::APP_ID]['signal:signal-instance-1:account'] = 'test-account';
+
+		/** @var Application */
+		$application = Server::get(Application::class);
+		$input = new ArrayInput([
+			'twofactorauth:gateway:status',
+			'-v' => true,
+		]);
+		$output = new ConsoleOutputSpy();
+		$application->loadCommands($input, $output);
+		$application->setAutoExit(false);
+
+		$exitCode = $application->run($input, $output);
+		$stdout = $output->fetch();
+
+		$this->assertSame(0, $exitCode);
+		$this->assertStringContainsString('Signal: configured', $stdout);
+		$this->assertStringContainsString('"instances"', $stdout);
+		$this->assertStringContainsString('"signal-instance-1"', $stdout);
 	}
 }
