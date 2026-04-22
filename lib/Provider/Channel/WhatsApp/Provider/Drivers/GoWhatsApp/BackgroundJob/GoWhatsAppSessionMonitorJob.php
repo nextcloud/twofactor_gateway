9e7fe2260bc7dc0df2c298533954332a634b937c
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2026-04-22 18:16:11 -0300

    refactor(whatsapp): move GoWhatsApp internals to driver namespace
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

--- lib/Provider/Channel/WhatsApp/Provider/Drivers/GoWhatsApp/BackgroundJob/GoWhatsAppSessionMonitorJob.php
+++ lib/Provider/Channel/WhatsApp/Provider/Drivers/GoWhatsApp/BackgroundJob/GoWhatsAppSessionMonitorJob.php
@@ -7,7 +7,7 @@ declare(strict_types=1);
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 
-namespace OCA\TwoFactorGateway\Provider\Channel\WhatsApp\BackgroundJob;
+namespace OCA\TwoFactorGateway\Provider\Channel\WhatsApp\Provider\Drivers\GoWhatsApp\BackgroundJob;
 
 use OCA\TwoFactorGateway\Provider\Channel\WhatsApp\Provider\Drivers\GoWhatsApp\SessionHealthService;
 use OCP\AppFramework\Utility\ITimeFactory;
--- lib/Provider/Channel/WhatsApp/Provider/Drivers/GoWhatsApp/Events/WhatsAppAuthenticationErrorEvent.php
+++ lib/Provider/Channel/WhatsApp/Provider/Drivers/GoWhatsApp/Events/WhatsAppAuthenticationErrorEvent.php
@@ -7,7 +7,7 @@ declare(strict_types=1);
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 
-namespace OCA\TwoFactorGateway\Provider\Channel\WhatsApp\Events;
+namespace OCA\TwoFactorGateway\Provider\Channel\WhatsApp\Provider\Drivers\GoWhatsApp\Events;
 
 use OCA\TwoFactorGateway\Events\AdminNotifiableEvent;
 use OCP\EventDispatcher\Event;
