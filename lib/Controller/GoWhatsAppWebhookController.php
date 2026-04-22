1485fbab4c9589414341be7725cd79e76add4169
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2026-04-22 18:27:40 -0300

    fix(openapi): keep webhook controller in standard namespace
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

--- lib/Controller/GoWhatsAppWebhookController.php
+++ lib/Controller/GoWhatsAppWebhookController.php
@@ -7,7 +7,7 @@ declare(strict_types=1);
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 
-namespace OCA\TwoFactorGateway\Provider\Channel\WhatsApp\Provider\Drivers\GoWhatsApp\Controller;
+namespace OCA\TwoFactorGateway\Controller;
 
 use OCA\TwoFactorGateway\Provider\Channel\WhatsApp\Provider\Drivers\GoWhatsApp\WebhookIngestionService;
 use OCP\AppFramework\Controller;
