9ceb5f7d51139fc1075b539e4d34cf106ad8ca7a
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2026-05-11 20:17:59 -0300

    chore(reuse): add SPDX header to lib/twofactor-gateway/composables/index.ts
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

--- src/lib/twofactor-gateway/composables/index.ts
+++ src/lib/twofactor-gateway/composables/index.ts
@@ -1,2 +1,5 @@
+// SPDX-FileCopyrightText: 2026 LibreCode coop and contributors
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 export * from './useGatewayApi.ts'
 export * from './useGatewayForm.ts'
