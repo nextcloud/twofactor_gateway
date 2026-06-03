b7a1d640464b80186543e02ceeafcac54a792527
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2026-06-03 00:35:25 -0300

    test: restore simple phpunit bootstrap
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

--- tests/php/bootstrap.php
+++ tests/php/bootstrap.php
@@ -6,7 +6,6 @@
  */
 
 use OCP\App\IAppManager;
-use OCP\IConfig;
 use OCP\Server;
 
 if (!defined('PHPUNIT_RUN')) {
@@ -16,15 +15,4 @@ if (!defined('PHPUNIT_RUN')) {
 require_once __DIR__ . '/../../../../lib/base.php';
 require_once __DIR__ . '/../../../../tests/autoload.php';
 
-$config = Server::get(IConfig::class);
-$appManager = Server::get(IAppManager::class);
-
-if ($config->getAppValue('twofactor_gateway', 'installed_version', '') === '') {
-	$config->setAppValue('twofactor_gateway', 'installed_version', '4.0.0-dev.0');
-}
-
-if (!in_array('twofactor_gateway', $appManager->getEnabledApps(), true)) {
-	$appManager->enableApp('twofactor_gateway', true);
-}
-
-$appManager->loadApp('twofactor_gateway');
+Server::get(IAppManager::class)->loadApp('twofactor_gateway');
