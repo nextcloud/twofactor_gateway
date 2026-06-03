0235f90cb986d220f22f8e2e335323dd9a38c4b3
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2026-06-03 00:26:58 -0300

    refactor: add field exposure enum
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

---
+++ lib/Provider/FieldExposure.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * SPDX-FileCopyrightText: 2026 LibreCode coop and contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+namespace OCA\TwoFactorGateway\Provider;
+
+enum FieldExposure: string {
+	case ADMIN = 'admin';
+	case DELEGATED = 'delegated';
+	case RUNTIME = 'runtime';
+	case NEVER = 'never';
+
+	public static function fromNullable(string|self|null $exposure): self {
+		if ($exposure instanceof self) {
+			return $exposure;
+		}
+
+		if ($exposure === null || $exposure === '') {
+			return self::ADMIN;
+		}
+
+		return self::tryFrom($exposure) ?? self::ADMIN;
+	}
+}
