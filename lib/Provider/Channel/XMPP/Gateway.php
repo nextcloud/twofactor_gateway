6529d92574dad89f6848c889af7a0e0410f6c07c
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2026-06-04 22:48:49 -0300

    refactor: drop redundant xmpp admin exposure
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

--- lib/Provider/Channel/XMPP/Gateway.php
+++ lib/Provider/Channel/XMPP/Gateway.php
@@ -12,7 +12,6 @@ namespace OCA\TwoFactorGateway\Provider\Channel\XMPP;
 use OCA\TwoFactorGateway\Exception\MessageTransmissionException;
 use OCA\TwoFactorGateway\PhoneNumberMask;
 use OCA\TwoFactorGateway\Provider\FieldDefinition;
-use OCA\TwoFactorGateway\Provider\FieldExposure;
 use OCA\TwoFactorGateway\Provider\Gateway\AGateway;
 use OCA\TwoFactorGateway\Provider\Settings;
 use OCP\IAppConfig;
@@ -54,28 +53,23 @@ class Gateway extends AGateway {
 				new FieldDefinition(
 					field: 'sender',
 					prompt: 'Please enter your sender XMPP-JID:',
-					exposure: FieldExposure::ADMIN,
 				),
 				new FieldDefinition(
 					field: 'password',
 					prompt: 'Please enter your sender XMPP password:',
-					exposure: FieldExposure::ADMIN,
 				),
 				new FieldDefinition(
 					field: 'server',
 					prompt: 'Please enter full path to access REST/HTTP API:',
-					exposure: FieldExposure::ADMIN,
 				),
 				new FieldDefinition(
 					field: 'username',
 					prompt: 'XMPP username (derived from sender JID)',
 					optional: true,
-					exposure: FieldExposure::ADMIN,
 				),
 				new FieldDefinition(
 					field: 'method',
 					prompt: 'Please enter 1 or 2 for XMPP sending option:',
-					exposure: FieldExposure::ADMIN,
 				),
 			],
 		);
