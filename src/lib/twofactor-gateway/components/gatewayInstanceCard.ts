d1f1b08ab6d908e98cf5aa4f1c7f3150df62fdff
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2026-06-06 14:57:22 -0300

    feat: add gateway instance card entry point
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

---
+++ src/lib/twofactor-gateway/components/gatewayInstanceCard.ts
@@ -0,0 +1,4 @@
+// SPDX-FileCopyrightText: 2026 LibreCode coop and contributors
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+export { default as GatewayInstanceCard } from '../../../components/GatewayInstanceCard.vue'
