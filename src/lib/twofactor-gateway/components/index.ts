4225e6a1ff9b34aa5a2ff3ab188b9cb873939544
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2026-06-06 14:57:22 -0300

    feat: re-export granular component entries
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

--- src/lib/twofactor-gateway/components/index.ts
+++ src/lib/twofactor-gateway/components/index.ts
@@ -1,9 +1,10 @@
 // SPDX-FileCopyrightText: 2026 LibreCode coop and contributors
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
-export { default as GatewayAdminSettings } from '../../../views/AdminSettings.vue'
-export { default as GatewayInstanceCard } from '../../../components/GatewayInstanceCard.vue'
-export { default as GatewayInstanceModal } from '../../../components/GatewayInstanceModal.vue'
-export { default as GatewayRoutingModal } from '../../../components/GatewayRoutingModal.vue'
-export { default as GatewaySection } from '../../../components/GatewaySection.vue'
-export { default as GatewayTestModal } from '../../../components/GatewayTestModal.vue'
+export * from './adminSettings.ts'
+export * from './gatewayManagement.ts'
+export * from './gatewayInstanceCard.ts'
+export * from './gatewayInstanceModal.ts'
+export * from './gatewayRoutingModal.ts'
+export * from './gatewaySection.ts'
+export * from './gatewayTestModal.ts'
