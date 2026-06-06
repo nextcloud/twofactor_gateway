50e45756a96cad4a52aa954b51834b3d68f7de4d
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2026-06-06 14:40:50 -0300

    feat: add reusable gateway component surface
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

---
+++ src/lib/twofactor-gateway/components/index.ts
@@ -0,0 +1,9 @@
+// SPDX-FileCopyrightText: 2026 LibreCode coop and contributors
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+export { default as GatewayAdminSettings } from '../../../views/AdminSettings.vue'
+export { default as GatewayInstanceCard } from '../../../components/GatewayInstanceCard.vue'
+export { default as GatewayInstanceModal } from '../../../components/GatewayInstanceModal.vue'
+export { default as GatewayRoutingModal } from '../../../components/GatewayRoutingModal.vue'
+export { default as GatewaySection } from '../../../components/GatewaySection.vue'
+export { default as GatewayTestModal } from '../../../components/GatewayTestModal.vue'
