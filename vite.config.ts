baca2d60ad3b8a7f41e234ef8651f62566c0fffa
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2026-06-07 22:55:31 -0300

    refactor(docs): relocate tooling files
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import { createAppConfig } from '@nextcloud/vite-config'
 import eslint from 'vite-plugin-eslint'
 import stylelint from 'vite-plugin-stylelint'
 import path from 'path'
-import { vueDocsBlockPlugin } from './tools/vueDocsBlockPlugin.js'
+import { vueDocsBlockPlugin } from './src/styleguide/vueDocsBlockPlugin.js'
 
 const isProduction = process.env.NODE_ENV === 'production'
 
